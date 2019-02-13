b715c8eb423feb4ce4bb20cef920dd408181a5b7
mirse <mirsent@outlook.com>
2019-02-13 16:25:37 +0800

    api：画册

--- Application/Home/Controller/ReaderController.class.php
+++ Application/Home/Controller/ReaderController.class.php
@@ -35,4 +35,30 @@ class ReaderController extends Controller {
             ->select();
         ajax_return(1, '充值记录', $data);
     }
+
+    ////////
+    // 画册 //
+    ////////
+
+    public function get_gather_info()
+    {
+        $cond['g.status'] = C('APPLY_P');
+        $data = D('Gather')->order('publish_time desc')->getGatherData($cond);
+        ajax_return(1, '画册', $data);
+    }
+
+    /**
+     * 发布画册
+     */
+    public function publish_gather()
+    {
+        $gather = D('Gather');
+        $gather->create();
+        $res = $gather->add();
+
+        if ($res === false) {
+            ajax_return(0, '发布画册失败');
+        }
+        ajax_return(1);
+    }
 }
