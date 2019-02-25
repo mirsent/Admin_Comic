3a405161b2be93e9da1146c35eeae00fca4e4c6c
mirse <mirsent@outlook.com>
2019-02-25 15:04:45 +0800

    画册点赞管理

--- Application/Admin/Controller/ReaderController.class.php
+++ Application/Admin/Controller/ReaderController.class.php
@@ -309,6 +309,66 @@ class ReaderController extends AdminBaseController{
 
 
 
+    /**************************************** 画册点赞 **************************************/
+
+    /**
+     * 获取点赞信息
+     */
+    public function get_gather_likes_info()
+    {
+        $ms = D('GatherLikes');
+
+        $cond['l.status'] = C('STATUS_Y');
+
+        $recordsTotal = $ms->alias('l')->where($cond)->count();
+
+        // 搜索
+        $search = I('search');
+        if (strlen($search)>0) {
+            $cond['gather_title|nickname'] = array('like', '%'.$search.'%');
+        }
+        $cond['gather_title'] = I('gather_title');
+        $cond['nickname'] = I('nickname');
+        $searchDate = I('search_date');
+        if ($searchDate) {
+            $cond['like_time'] = array('BETWEEN', [$searchDate.' 00:00:00', $searchDate.' 23:59:59']);
+        }
+
+        $recordsFiltered = $ms->getLikesNumber($cond);
+
+        // 排序
+        $orderObj = I('order')[0];
+        $orderColumn = $orderObj['column']; // 排序列，从0开始
+        $orderDir = $orderObj['dir'];       // ase desc
+        if(isset(I('order')[0])){
+            $i = intval($orderColumn);
+            switch($i){
+                case 0: $ms->order('gather_title '.$orderDir); break;
+                case 1: $ms->order('nickname '.$orderDir); break;
+                case 2: $ms->order('like_time '.$orderDir); break;
+                default: break;
+            }
+        } else {
+            $ms->order('like_time desc');
+        }
+
+        // 分页
+        $start = I('start');  // 开始的记录序号
+        $limit = I('limit');  // 每页显示条数
+        $page = I('page');    // 第几页
+
+        $infos = $ms->page($page, $limit)->getLikesData($cond);
+
+        echo json_encode(array(
+            "draw" => intval(I('draw')),
+            "recordsTotal" => intval($recordsTotal),
+            "recordsFiltered" => intval($recordsFiltered),
+            "data" => $infos
+        ), JSON_UNESCAPED_UNICODE);
+    }
+
+
+
 
     /**************************************** 积分详情 **************************************/
 
--- Application/Common/Model/GatherLikesModel.class.php
+++ Application/Common/Model/GatherLikesModel.class.php
@@ -8,4 +8,27 @@ class GatherLikesModel extends BaseModel{
         array('like_time','get_datetime',1,'callback')
     );
 
+    public function getLikesNumber($cond=[])
+    {
+        $data = $this
+            ->alias('l')
+            ->join('__GATHER__ g ON g.id = l.gather_id')
+            ->join('__READER__ r On r.id = l.reader_id')
+            ->where(array_filter($cond))
+            ->count();
+        return $data;
+    }
+
+    public function getLikesData($cond=[])
+    {
+        $data = $this
+            ->alias('l')
+            ->join('__GATHER__ g ON g.id = l.gather_id')
+            ->join('__READER__ r On r.id = l.reader_id')
+            ->field('l.*,gather_title,r.nickname')
+            ->where(array_filter($cond))
+            ->select();
+        return $data;
+    }
+
 }
