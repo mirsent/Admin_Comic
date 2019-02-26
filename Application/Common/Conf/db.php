96126ee9830d050ec4cfedf541ef11bce13bca4a
mirse <mirsent@outlook.com>
2019-02-26 16:06:00 +0800

    更新ignore

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'DB_TYPE'    => 'mysql',
+    'DB_HOST'    => '127.0.0.1',
+    'DB_NAME'    => 'comic',
+    'DB_USER'    => 'root',
+    'DB_PWD'     => 'root',
+    // 'DB_HOST'    => '60.205.210.31',
+    // 'DB_NAME'    => 'comic',
+    // 'DB_USER'    => 'root',
+    // 'DB_PWD'     => 'HNW5btHJOB6J7UJM6Luk3',
+    'DB_PORT'    => '3306',
+    'DB_PREFIX'  => '',
+    'DB_CHARSET' =>  'utf8mb4',
+);
